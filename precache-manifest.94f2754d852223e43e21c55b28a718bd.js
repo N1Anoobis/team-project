db1a93dcf18abd155c16ff099e5add47d1dd10cd
N1Anoobis <mrbond84@hotmail.com>
2020-08-14 14:34:15 +0200

    Updates

---
+++ precache-manifest.94f2754d852223e43e21c55b28a718bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4db62240e0a7a6252100de5b0bc8a13",
+    "url": "/team-project/index.html"
+  },
+  {
+    "revision": "9bd0b8cba97d0326abda",
+    "url": "/team-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8314a4861b7c8cb8eb63",
+    "url": "/team-project/static/css/main.d4a20058.chunk.css"
+  },
+  {
+    "revision": "9bd0b8cba97d0326abda",
+    "url": "/team-project/static/js/2.21a6f007.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/team-project/static/js/2.21a6f007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8314a4861b7c8cb8eb63",
+    "url": "/team-project/static/js/main.7321d870.chunk.js"
+  },
+  {
+    "revision": "baa1a90138e174a0469a",
+    "url": "/team-project/static/js/runtime-main.912dc172.js"
+  }
+]);
